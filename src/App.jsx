89dd9872448dc04025630ce1f00afa09abe09f39
Samson Cheung <samsonc89@yahoo.com>
2023-05-03 15:38:41 -0700

    Create header element

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 function App() {
   return (
-    <form className="new-item-form">
-      <div className="form-row">
-        <label htmlFor="item">New Item</label>
-        <input type="text" id="item"></input>
-      </div>
-      <button className="btn">Adds</button>
-    </form>
+    <>
+      <form className="new-item-form">
+        <div className="form-row">
+          <label htmlFor="item">New Item</label>
+          <input type="text" id="item"></input>
+        </div>
+        <button className="btn">Adds</button>
+      </form>
+      <h1 className="header">Todo List</h1>
+    </>
   );
 }
 
